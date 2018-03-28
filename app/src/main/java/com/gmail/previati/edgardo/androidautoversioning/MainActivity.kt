ddeb3399225bef59a84f175ab9a1af817592c6bb
Edgardo Previati <edgardo@thehouseofmouse.net>
2018-03-28 14:42:35 +0200

    working on configuration

--- app/src/main/java/com/gmail/previati/edgardo/androidautoversioning/MainActivity.kt
+++ app/src/main/java/com/gmail/previati/edgardo/androidautoversioning/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.gmail.previati.edgardo.androidautoversioning
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        text_version_code.text = BuildConfig.VERSION_CODE.toString()
+        text_version_name.text = BuildConfig.VERSION_NAME
     }
 }
